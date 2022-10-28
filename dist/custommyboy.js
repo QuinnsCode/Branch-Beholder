1c75e45861a69e8853ac9c1ba5b5414111d17fd3
QuinnsCode <108157799+QuinnsCode@users.noreply.github.com>
2022-10-28 10:31:09 -0700

    Update custommyboy.js

--- dist/custommyboy.js
+++ dist/custommyboy.js
@@ -1,45 +1,45 @@
 <script type="text/javascript">
     console.log("Made it to the script");
-    engraver.init("preview-canvas");
+//     engraver.init("preview-canvas");
 
-    console.log("engraver initialized")
-    engraver.setProduct('f9e7abfe-e4c3-4541-9b3f-563289994ba2').then(function () {
-      // engraver.setImage(1, "http://i.imgur.com/9S2IPKz.jpg").then(function(){
-      // });
-    });
-    console.log("Made to set product")
-    function showUploadedImage(placeholderId, fileInputId) {
-      var file = $('#' + fileInputId)[0].files[0];
-      var reader = new FileReader();
+//     console.log("engraver initialized")
+//     engraver.setProduct('f9e7abfe-e4c3-4541-9b3f-563289994ba2').then(function () {
+//       // engraver.setImage(1, "http://i.imgur.com/9S2IPKz.jpg").then(function(){
+//       // });
+//     });
+//     console.log("Made to set product")
+//     function showUploadedImage(placeholderId, fileInputId) {
+//       var file = $('#' + fileInputId)[0].files[0];
+//       var reader = new FileReader();
 
-      reader.addEventListener("load", function () {
-        engraver.setImage(placeholderId, reader.result);
-      }, false);
+//       reader.addEventListener("load", function () {
+//         engraver.setImage(placeholderId, reader.result);
+//       }, false);
 
-      if (file) {
-        reader.readAsDataURL(file);
-      }
-    }
-    function previewVector(placeholderId, fileInputId) {
-      var file = $('#' + fileInputId)[0].files[0];
-      var reader = new FileReader();
+//       if (file) {
+//         reader.readAsDataURL(file);
+//       }
+//     }
+//     function previewVector(placeholderId, fileInputId) {
+//       var file = $('#' + fileInputId)[0].files[0];
+//       var reader = new FileReader();
 
-      reader.addEventListener("load", function () {
-        engraver.setVector(placeholderId, reader.result);
-      }, false);
+//       reader.addEventListener("load", function () {
+//         engraver.setVector(placeholderId, reader.result);
+//       }, false);
 
-      if (file) {
-        reader.readAsDataURL(file);
-      }
-    }
-    function processText(textId, value) {
-      engraver.setText(textId, value);
-    }
-    function changeWood(dynamicId, value) {
-      engraver.setPresetImage(dynamicId, parseInt(value));
-      engraver.setFontColor(1, parseInt(value));
-    }
-    function changeFont(textId, value) {
-      engraver.setFont(textId, parseInt(value));
-    }
+//       if (file) {
+//         reader.readAsDataURL(file);
+//       }
+//     }
+//     function processText(textId, value) {
+//       engraver.setText(textId, value);
+//     }
+//     function changeWood(dynamicId, value) {
+//       engraver.setPresetImage(dynamicId, parseInt(value));
+//       engraver.setFontColor(1, parseInt(value));
+//     }
+//     function changeFont(textId, value) {
+//       engraver.setFont(textId, parseInt(value));
+//     }
   </script>
